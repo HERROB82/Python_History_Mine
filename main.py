e146e59e7d21c55fc8565fc0b3eb351567ca9656
HERROB82 <hrz.salemme@gmail.com>
2022-12-03 12:24:08 +0100

    Updated_4

--- main.py
+++ main.py
@@ -49,28 +49,7 @@
 
 
 
-## BUCLE WHILE (Mientras)
-  # Se basa en repetir un bloque a partir de evaluar una condicion logica, siempre que esta sea TRUE
-  # Queda en manos del programador decidir el momentoen que la condicion cambie a False para hacer que el bucel WHILE finalice
 
-# ITERACIONES
-  # Iterar significa realizar una accion varias veces. Cada vez que se repite se denomina iteracion.
-
-#contador = 1
-#while contador <= 5:
-  #print(contador)
-  #contador += 1 #contador = contador + 1
-#print("continuamos con nuestro codigo")
-
-# Sentencias ELSE en bucle while
-# Se encadena al While para ejecutar un bloque de codigo una vez la condicion ya no devuelve True (normalmente al final)
-
-#numero = 1
-#while numero <= 5:
-  #numero += 1
-  #print("numero =",numero)
-#else:
-  #print("Fin del while -> numero =",numero)
 
 ## Instruccion BREAK
 # Sirve para "romper" la ejecucion del WHILE en cualquier momento. No se ejecutara el ELSE, ya que este solo se llama al finalizar la operacion.
